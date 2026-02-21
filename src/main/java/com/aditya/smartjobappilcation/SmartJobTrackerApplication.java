80c5e7ad0404b960868bb2051d0aba287b945700
Aditya Deshpande <adityacd181@gmail.com>
2026-02-21 14:48:32 +0530

    initial setup done

---
+++ src/main/java/com/aditya/smartjobappilcation/SmartJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.smartjobappilcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartJobTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/smartjobappilcation/SmartJobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.smartjobappilcation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartJobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
